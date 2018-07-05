9f68656e2c01c457c4a525c5c55b141d93cc9d7d
Per Böhlin <per.bohlin@devconsoft.se>
2018-07-05 23:56:30 +0200

    Added systest for rm non-existing store.

--- systest/tests/test_rm.py
+++ systest/tests/test_rm.py
@@ -4,3 +4,7 @@ def test_rm_long_opt_help(pycred):
 
 def test_rm_short_opt_help(pycred):
     pycred('rm -h')
+
+
+def test_rm_none_existing_store_gives_exit_code_2(pycred):
+    pycred('rm non-existing-store user', expected_exit_code=2)
